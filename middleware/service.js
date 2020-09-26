a64b1b29ed9ccc0940a8439c1948cc7391040abc
tump <abcd498936590@gmail.com>
2020-09-26 22:05:18 +0800

    增加关闭服务路径识别-app路径，public路径

--- middleware/service.js
+++ middleware/service.js
@@ -13,9 +13,11 @@ module.exports = (ctx, next) => {
 
 		let isManage = /^\/manage/.test(url);  // 后台管理系统文件 + 后台ajax接口
 		let isUpload = /^\/upload/.test(url);  // 上传文件静态目录
+		let isApp = /^\/app/.test(url);        // app接口
+		let isPublic = /^\/public/.test(url);  // 通用接口
 
 		// 如果不是后台
-		if(!isAllowAdopt && !isManage && !isUpload){
+		if(!isAllowAdopt && !isManage && !isUpload && !isApp && !isPublic){
 			ctx.status = 403;
 			ctx.body = '';
 			return
